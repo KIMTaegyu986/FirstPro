d9f4815efbf3d277823bb7972fd1e1a9434d02ce
KIMTaegyu986 <33xorb@naver.com>
2022-04-05 22:03:51 +0900

    1

--- 0329_DB/src/t1_DBCConnection/DBCConnectionRun.java
+++
@@ -1,8 +0,0 @@
-package t1_DBCConnection;
-
-public class DBCConnectionRun {
-	public static void main(String[] args) {
-		new DBCConnection();
-		
-	}
-}
--- 0329_DB/src/t1_DBConnection/DBConnectionRun.java
+++
@@ -1,7 +0,0 @@
-package t1_DBConnection;
-
-public class DBConnectionRun {
-	public static void main(String[] args) {
-		new DBConnection();
-	}
-}
